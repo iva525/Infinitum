12e1be3770a28ba8f379868484e035729d56314d
iva525 <macks6315@gmail.com>
2026-06-13 06:44:30 +0800

    Add initial app.js with basic functions and constants

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const APP_NAME = "Infinitum";
+const APP_VERSION = "1.0";
+
+function go(page){
+
+    location.href = page;
+
+}
+
+function getUser(){
+
+    return localStorage.getItem(
+        "username"
+    );
+
+}
+
+function isAdmin(){
+
+    return localStorage.getItem(
+        "admin"
+    ) === "true";
+
+}
