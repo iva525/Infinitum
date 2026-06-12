bc67856a80159cbd34f21362d63fe833556cad6b
iva525 <macks6315@gmail.com>
2026-06-12 15:29:10 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "infinitum-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/search.html",
+    "/login.html",
+    "/profile.html",
+    "/admin.html",
+    "/ai.html",
+    "/settings.html",
+    "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
