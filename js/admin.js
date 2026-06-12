e90377dd09029794fec9210f96434b6aa47da8a7
iva525 <macks6315@gmail.com>
2026-06-13 06:30:52 +0800

    Create admin.js

---
+++ js/admin.js
@@ -0,0 +1,59 @@
+function loadAdminStats(){
+
+    let users = JSON.parse(
+        localStorage.getItem("users") || "[]"
+    );
+
+    let searches = JSON.parse(
+        localStorage.getItem("searchHistory") || "[]"
+    );
+
+    let usersCount =
+    document.getElementById("usersCount");
+
+    let searchesCount =
+    document.getElementById("searchesCount");
+
+    if(usersCount){
+        usersCount.textContent =
+        users.length;
+    }
+
+    if(searchesCount){
+        searchesCount.textContent =
+        searches.length;
+    }
+
+}
+
+function loadUsers(){
+
+    let users = JSON.parse(
+        localStorage.getItem("users") || "[]"
+    );
+
+    let list =
+    document.getElementById("usersList");
+
+    if(!list) return;
+
+    list.innerHTML = "";
+
+    users.forEach(user=>{
+
+        list.innerHTML += `
+        <div class="user-card">
+            ${user.login}
+        </div>
+        `;
+
+    });
+
+}
+
+window.onload = function(){
+
+    loadAdminStats();
+    loadUsers();
+
+};
